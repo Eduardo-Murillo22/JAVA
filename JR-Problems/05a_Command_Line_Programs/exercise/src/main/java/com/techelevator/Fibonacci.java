bc916d41964e8793d931379c89e2046abe7f2dca
Eduardo Murillo <eds445388@gmail.com>
2024-02-19 22:27:25 -0800

    solved

--- JR-Problems/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ JR-Problems/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,31 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+//		Fn = F n-1 + F n-2
+
+
+		System.out.println("Fibinachi : ");
+		Scanner input = new Scanner(System.in);
+		int count = input.nextInt();
+
+		int firstTerm = 0, secondTerm = 1;
+		System.out.println("Fibonacci Series till " + count + " terms:");
+
+		for (int i = 1; i <= count; ++i) {
+			System.out.print(firstTerm + " ");
+
+			// compute the next term
+			int nextTerm = firstTerm + secondTerm;
+			firstTerm = secondTerm;
+			secondTerm = nextTerm;
+			i = firstTerm;
+		}
+
+
 
 	}
 
